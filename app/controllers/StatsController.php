2ee8428ac72eb6881bb879b97c833718d7e425bf
johanfalk <johanjobbfalk@hotmail.com>
2014-08-13 17:39:27 +0200

    Added Stats controller for stats page

--- app/controllers/StatsController.php
+++ app/controllers/StatsController.php
@@ -4,8 +4,8 @@ class StatsController extends BaseController {
 
 	public function showPlayerStats()
 	{
-		//return Response::json(SteamApi::getMatchDetails('828388121'));
-	
-		return Response::json(SteamApi::getMatchHistory('23654985'));
+		$steamApi = new SteamApi();
+
+		return Response::json($steamApi->getMatchDetails('828388121'));
 	}
 }
