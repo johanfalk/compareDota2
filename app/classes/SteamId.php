1dcff4e8ed044d43f5000bdae625cdff1bc1ebb9
johanfalk <johanjobbfalk@hotmail.com>
2014-08-14 13:35:09 +0200

    Updated so its easier to get 32 and 64 bit ID

---
+++ app/classes/SteamId.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Convert 64bit IDs to 32bit or the other way around. 
+ */
+class SteamId
+{
+
+	public $sixtyFour;
+
+	public $thirtyFour;
+
+	/**
+	 * @param int $id
+	 * @return string 
+	 */
+	function __construct($id)
+	{
+	    if (strlen($id) === 17)
+	    {
+	    	$this->sixtyFour = $id;
+
+	        $this->thirtyFour = substr($id, 3) - 61197960265728;
+	    }
+	    else
+	    {
+	    	$this->thirtyFour = $id;
+
+	        $sixtyFour = '765'.($id + 61197960265728);
+	    }
+
+	    return $this->sixtyFour;
+	}
+}
